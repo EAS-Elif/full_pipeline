71be6aaa27eae6a4db963d2fea1a9e2ca3700494
EAS-ELIF <elif@enterpriseautomation.co.uk>
2023-07-14 17:15:37 +0100

    updated jenkins.yaml.j2

--- terraform/data.tf
+++ terraform/data.tf
@@ -26,10 +26,7 @@ data "vsphere_virtual_machine" "template" {
   name          = var.vsphere_virtual_machine
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-# data "vsphere_virtual_machine" "template" {
-#   name          = var.vsphere_virtual_machine
-#   datacenter_id = data.vsphere_datacenter.dc.id
-# }
+
 data "vsphere_host" "host" {
   name          = var.vsphere_host
   datacenter_id = data.vsphere_datacenter.dc.id
